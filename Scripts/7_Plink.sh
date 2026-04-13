077b208cde1baf098295e453dbf703e7be410bea
mbxsc9 <mbxsc9@nottingham.ac.uk>
2026-04-13 23:08:55 +0100

    Add SLURM script for PLINK analysis
    This script sets up a SLURM job for running PLINK analysis on a VCF file, including commands for converting the VCF to BED format and calculating missing genotype data.

---
+++ Scripts/7_Plink.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=50g
+#SBATCH --time=1:00:00
+#SBATCH --job-name=plink
+#SBATCH --output=/gpfs01/share/BioinfMSc/life4136_2526/rotation3/group4/Scripts/Logs/slurm-%x-%j.out
+#SBATCH --error=/gpfs01/share/BioinfMSc/life4136_2526/rotation3/group4/Scripts/Logs/slurm-%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mbxsc9@nottingham.ac.uk
+
+source $HOME/.bash_profile
+conda activate rotation3
+
+INPUT=/share/BioinfMSc/life4136_2526/rotation3/group4/doggies_snps_imputed.vcf.gz
+#cd /share/BioinfMSc/life4136_2526/rotation3/group4/plink_data
+OUTDIR=/share/BioinfMSc/life4136_2526/rotation3/group4/plink_data
+
+plink --vcf "$INPUT" --double-id --allow-extra-chr --make-bed --out "$OUTDIR"/doggies_raw
+
+plink --bfile "$OUTDIR"/doggies_raw --missing --allow-extra-chr --out "$OUTDIR"/doggies_missing
+
+conda deactivate
