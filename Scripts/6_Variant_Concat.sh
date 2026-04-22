f778d7d9bd77fa8d398fa15e7881bb0de23ee423
mbxsc9 <mbxsc9@nottingham.ac.uk>
2026-04-22 15:03:15 +0100

    Using bcftool to merge multiple VCF files into one concatenated VCF
    Updated SLURM job parameters and output paths.

--- Scripts/6_Variant_Concat.sh
+++ Scripts/6_Variant_Concat.sh
@@ -2,11 +2,14 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=32
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=32g
-#SBATCH --time=01:00:00
+#SBATCH --time=2:00:00
 #SBATCH --job-name=variant_concat
-#SBATCH --output=index_ref.out
-#SBATCH --error=index_ref.err
+#SBATCH --mail-user=XXX@nottingham.ac.uk
+#SBATCH --output=XXX/slurm-%x-%j.out
+#SBATCH --error=XXX/slurm-%x-%j.err
+#SBATCH --mail-type=ALL
 
 # Load Conda Environment
 source $HOME/.bash_profile
@@ -17,4 +20,4 @@ module load bcftools-uoneasy/1.18-GCC-13.2.0
 
 # Concatenate all vcf files into a single vcf file
 bcftools concat --file-list ../vcf.list.txt -Oz --output ../vcf/dog.vcf.gz --threads 32
-bcftools index ../vcf/dog.vcf.gz --threads 32
+bcftools index ../vcf/dog.vcf.gz --threads 32 # Index the merged VCF file
