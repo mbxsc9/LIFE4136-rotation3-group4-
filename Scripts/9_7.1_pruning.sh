cd7c70af6d0a4795ca68950476f4e389e7cfb37f
mbxsc9 <mbxsc9@nottingham.ac.uk>
2026-04-23 00:05:16 +0100

    Modify SLURM script paths and email placeholders
    Updated output and error log paths, email address, and input/output directories to use placeholders.

--- Scripts/9_7.1_pruning.sh
+++ Scripts/9_7.1_pruning.sh
@@ -6,20 +6,26 @@
 #SBATCH --mem=50g
 #SBATCH --time=1:00:00
 #SBATCH --job-name=plink_gwas
-#SBATCH --output=/gpfs01/share/BioinfMSc/life4136_2526/rotation3/group4/Scripts/Logs/slurm-%x-%j.out
-#SBATCH --error=/gpfs01/share/BioinfMSc/life4136_2526/rotation3/group4/Scripts/Logs/slurm-%x-%j.err
+#SBATCH --output=XXX/slurm-%x-%j.out
+#SBATCH --error=XXX/slurm-%x-%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=mbxsc9@nottingham.ac.uk
+#SBATCH --mail-user=XXX@nottingham.ac.uk
 
+# Activate conda environment
 source $HOME/.bash_profile
 conda activate rotation3
 
 #loading the plink module
 module load plink-uoneasy/2.00a3.7-foss-2023a
 
-INPUT=/share/BioinfMSc/life4136_2526/rotation3/group4/plink_data
-OUTPUT=/share/BioinfMSc/life4136_2526/rotation3/group4/plink_data/LD_pruning
+# Define input
+INPUT=XXX/plink_data
+OUTPUT=XXX/plink_data/LD_pruning
 
+# plink checks for 
+# Pair of SNPs with a correlation coefficient of more than 0.2 are removed. 
 plink --bfile "$INPUT"/doggies_raw \
  --allow-extra-chr --indep-pairwise 50 5 0.2 \
  --out "$OUTPUT"/prune
+
+ conda deactivate 
