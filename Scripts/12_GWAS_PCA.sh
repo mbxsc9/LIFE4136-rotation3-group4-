11bea69a8801d057533c718bc00e1197ad9590aa
mbxsc9 <mbxsc9@nottingham.ac.uk>
2026-04-22 23:57:09 +0100

    Modify SLURM job configuration for PCA analysis
    Updated job name and output paths for PCA analysis. Adjusted input and output variables for new file structure.

--- Scripts/12_GWAS_PCA.sh
+++ Scripts/12_GWAS_PCA.sh
@@ -5,23 +5,27 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=50g
 #SBATCH --time=1:00:00
-#SBATCH --job-name=plink_gwas
-#SBATCH --output=/gpfs01/share/BioinfMSc/life4136_2526/rotation3/group4/Scripts/Logs/slurm-%x-%j.out
-#SBATCH --error=/gpfs01/share/BioinfMSc/life4136_2526/rotation3/group4/Scripts/Logs/slurm-%x-%j.err
+#SBATCH --job-name=plink_gwas_PCA
+#SBATCH --output=XXX/slurm-%x-%j.out
+#SBATCH --error=XXX/slurm-%x-%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=mbxsc9@nottingham.ac.uk
+#SBATCH --mail-user=XXX@nottingham.ac.uk
 
+# Activating conda environment
 source $HOME/.bash_profile
 conda activate rotation3
 
 #loading the plink module
 module load plink-uoneasy/2.00a3.7-foss-2023a
 
-INPUT=/share/BioinfMSc/life4136_2526/rotation3/group4/plink_data
-TXT=/share/BioinfMSc/life4136_2526/rotation3/group4/plink_data/gwas_plink
-OUTPUT=/share/BioinfMSc/life4136_2526/rotation3/group4/plink_data/LD_pruning
+# Define input
+INPUT=XXX/plink_data
+TXT=XXX/plink_data/gwas_plink
+OUTPUT=XXX/plink_data/LD_pruning
 
-
+# Run GWAS using plink's linear model.
+# LD pruning to remove SNPs that are highly correlated with each other. left with independent markers suitable for population structure analysis.
+# PCA on pruned SNPs. calculate 20 PCs. 
 plink --bfile "$INPUT"/doggies_raw \
  --allow-extra-chr --allow-no-sex --pheno "$TXT"/pheno_doggies_height.txt \
  --covar "$OUTPUT"/pca20.eigenvec \
