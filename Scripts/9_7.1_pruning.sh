0b774a4797afe16405a4da9110eb7003c840aaf8
mbxsc9 <mbxsc9@nottingham.ac.uk>
2026-04-23 00:08:51 +0100

    Add PCA calculation for first 20 PCs
    Added PCA calculation step after SNP pruning.

--- Scripts/9_7.1_pruning.sh
+++ Scripts/9_7.1_pruning.sh
@@ -28,4 +28,9 @@ plink --bfile "$INPUT"/doggies_raw \
  --allow-extra-chr --indep-pairwise 50 5 0.2 \
  --out "$OUTPUT"/prune
 
+# Plink calculates the first 20 PCs using only the purned SNPs.
+plink --bfile "$INPUT"/doggies_raw \
+ --allow-extra-chr --extract "$OUTPUT"/prune.prune.in \
+ --pca 20 --out "$OUTPUT"/pca20 # calculate 20 PCs
+
  conda deactivate 
