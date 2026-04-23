8ad7910dc30bf5e399a02e3c0d8e853cf2a1d480
mbxsc9 <mbxsc9@nottingham.ac.uk>
2026-04-23 18:16:07 +0100

    LD pruning
    Updated input variable and added comments for clarity.

--- Scripts/8.0 pruning.sh
+++ Scripts/8.0 pruning.sh
@@ -19,15 +19,19 @@ conda activate rotation3
 module load plink-uoneasy/2.00a3.7-foss-2023a
 
 # Define input
+# INPUT1=XXX/plink_data/plink_QC_Genotype
 INPUT=XXX/plink_data
 OUTPUT=XXX/plink_data/LD_pruning
 
+
 # plink checks for 
 # Pair of SNPs with a correlation coefficient of more than 0.2 are removed. 
+# use "$INPUT1"/doggies_qc for better analysis.
 plink --bfile "$INPUT"/doggies_raw \
  --allow-extra-chr --indep-pairwise 50 5 0.2 \
  --out "$OUTPUT"/prune
 
+# use "$INPUT1"/doggies_qc for better analysis.
 # Plink calculates the first 20 PCs using only the purned SNPs.
 plink --bfile "$INPUT"/doggies_raw \
  --allow-extra-chr --extract "$OUTPUT"/prune.prune.in \
